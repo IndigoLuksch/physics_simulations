51d1d18b926d316c73160b9bf8f46a395a2659fd
IndigoLuksch <indigoluksch@proton.me>
2025-11-20 11:02:14 +0000

    ..

--- config.py
+++ config.py
@@ -36,6 +36,23 @@ material = 'Al'
 
 if material == 'Al':
     #PURE ALUMINIUM
+    #phase diagram
+    T_melt = 660 + 273
+    #k_S = 220           #W/(m*K) (thermal conductivity) (assume constant within each phase)
+    #k_L = 100
+    #heat flow -- note thermal conductivity is for temp gradient, heat transfer coefficient is for interface
+    k_LS = 230          #thermal conductivity: S/L<-->S/L [7]
+    h_Sm = 0.5          #heat transfer coefficient: S<-->mould [6]
+    h_Lm = 1.3          #heat transfer coefficient: L<-->mould [6]
+
+    shc = 921           #J/(K kg)
+    #energy
+    dH_LS = 1067000000  #J/(m^3)
+    E_srf_SS = 0.3         #surface energy between two grains (assume independent of grain angle) (assume E_srf_LS = 0) (source: [1])
+    E_srf_SS = 2 #delete this
+    E_srf_LS = 0.2 * E_srf_SS
+    #other
+    density = 2700      #kg/(m^3)
     n = 5.979e28 * pix_dim**3  #atoms per pixel (mm^3)
     #casting parameters
     T_initial = 900 +273 #change to 1500°C
